f5aa1776edc25d2c38525b85e553ca6945776788
Daniel Dehne <danny.dehne@gmail.com>
2025-10-20 20:32:12 +0200

    studio: add dark theme

--- nodeflux_studio/src/main.cpp
+++ nodeflux_studio/src/main.cpp
@@ -1,6 +1,8 @@
 #include "MainWindow.h"
 #include <QApplication>
 #include <QSurfaceFormat>
+#include <QPalette>
+#include <QStyleFactory>
 
 int main(int argc, char *argv[]) {
   // Set up OpenGL surface format BEFORE creating QApplication
@@ -14,6 +16,25 @@ int main(int argc, char *argv[]) {
 
   // Create the Qt application
   QApplication app(argc, argv);
+
+  // Set up dark mode palette
+  QPalette darkPalette;
+  darkPalette.setColor(QPalette::Window, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::WindowText, Qt::white);
+  darkPalette.setColor(QPalette::Base, QColor(25, 25, 25));
+  darkPalette.setColor(QPalette::AlternateBase, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ToolTipBase, Qt::white);
+  darkPalette.setColor(QPalette::ToolTipText, Qt::white);
+  darkPalette.setColor(QPalette::Text, Qt::white);
+  darkPalette.setColor(QPalette::Button, QColor(53, 53, 53));
+  darkPalette.setColor(QPalette::ButtonText, Qt::white);
+  darkPalette.setColor(QPalette::BrightText, Qt::red);
+  darkPalette.setColor(QPalette::Link, QColor(42, 130, 218));
+  darkPalette.setColor(QPalette::Highlight, QColor(42, 130, 218));
+  darkPalette.setColor(QPalette::HighlightedText, Qt::black);
+  
+  app.setPalette(darkPalette);
+  app.setStyle(QStyleFactory::create("Fusion")); // Fusion style works best with dark themes
 
   // Set application metadata (shows in window title, about dialogs, etc.)
   app.setApplicationName("NodeFlux Studio");
